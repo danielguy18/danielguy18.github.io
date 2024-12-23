13abe79e4af7e8a74bd71dbe159dd3e5f105268a
Daniel <danielguy181@gmail.com>
2024-12-23 00:06:23 -0800

    change 4 joop

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('clickMe');
     button.addEventListener('click', () => {
-        alert('Hi Drew!');
+        alert('Hi Joop!!');
     });
 });
