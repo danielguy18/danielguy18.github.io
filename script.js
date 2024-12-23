7295022c0e0b036d0411b10d79132a220534158e
Daniel <danielguy181@gmail.com>
2024-12-22 22:49:17 -0800

    cool

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('clickMe');
     button.addEventListener('click', () => {
-        alert('Button clicked!');
+        alert('Hi Drew!');
     });
 });
